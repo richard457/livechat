47965f52fba10fdfbc5bf251a836685e11ebd9bc
Richard Muragijimana <beastar457@gmail.com>
2023-06-02 20:31:36 +0200

    getting live chat

--- predeploy.sh
+++ predeploy.sh
@@ -2,3 +2,5 @@
 
 # remove the version hash from our base javascript file for a stable URL
 find build/static/js -name "main.*.js" -exec mv '{}' build/static/js/main.js \;
+
+# https://www.bitovi.com/blog/how-to-create-a-web-component-with-create-react-app
