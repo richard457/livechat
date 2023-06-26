5fffcdd4799b0c4dd177d2c8a9bc553f07736141
Richard Muragijimana <beastar457@gmail.com>
2023-06-26 06:08:57 +0200

    fix bad generation of main

--- predeploy.sh
+++ predeploy.sh
@@ -2,3 +2,4 @@
 
 # remove the version hash from our base javascript file for a stable URL
 find build/static/js -name "main.*.js" -exec mv '{}' build/static/js/main.js \;
+find build -type f -name "*.html" -exec sed -i 's/main\.[a-zA-Z0-9]*\.js/main.js/g' {} +
